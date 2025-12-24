99a61cc81cd1aacbf3e05050c1a85b6a2dc4d865
Rusty <ruslan.bone@outlook.com>
2025-12-24 18:56:32 +0000

    Fixed the naming base path, cause I didn't see... lmao

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Rusty-to-Wizard.github.io',
-	base: '/homelab-docs',
+	base: '/HomelabDoc',
 	integrations: [
 		starlight({
 			title: 'Homelab Documentation',
